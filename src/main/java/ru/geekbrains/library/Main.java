154d20fea184521f01fcd0e3db9083ee7d8598c3
Sergey Efremov <sergey.efremov@smartbics.com>
2021-01-16 12:59:59 +0300

    Add dependencies and Spring config

--- src/main/java/ru/geekbrains/library/Main.java
+++ src/main/java/ru/geekbrains/library/Main.java
@@ -2,20 +2,22 @@ package ru.geekbrains.library;
 
 import java.util.Arrays;
 
-import ru.geekbrains.library.repository.BookRepositoryInMemoryImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.geekbrains.library.service.BookCrudService;
-import ru.geekbrains.library.service.BookCrudServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Первая реализация сервиса.
-        // Код содержит только бизнес-логику и ничего больше
-        BookCrudService service = new BookCrudServiceImpl(new BookRepositoryInMemoryImpl());
+        // Spring сам будет создавать сервисы и внедрять нужные зависимости.
+        // Нам следует только описать в xml где ему искать информацию о наших классах
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("aop-config.xml");
+        // а нужный сервис Spring нам достанет из контекста
+        BookCrudService service = (BookCrudService) context.getBean("bookService");
 
         System.out.println(service.create("978-5-17-126976-0", Arrays.asList("А. Стругацкий", "Б. Стругацкий"), "Парень из преисподней", 224, "М.: АСТ"));
         System.out.println(service.create("978-5-04-101126-0", Arrays.asList("М. Дяченко", "С. Дяченко"), "Vita Nostra", 576, "М.: Эксмо"));
         System.out.println(service.get("978-5-17-126976-0"));
-//        System.out.println(service.get("978-5-17-126976-10")); // Not found exception
     }
 }
